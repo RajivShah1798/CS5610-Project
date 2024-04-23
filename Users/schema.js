1a13e68837b5ef66ae50e41bca1d5169f05d63f9
Rajiv Shah <shah.rajiv1702@gmail.com>
2024-04-23 19:50:09 -0400

    reduced Mandatory fields for new User Creation

--- Users/schema.js
+++ Users/schema.js
@@ -23,12 +23,10 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
       unique: true,
     },
     name: {
       type: String,
-      required: true,
     },
     password: {
       type: String,
